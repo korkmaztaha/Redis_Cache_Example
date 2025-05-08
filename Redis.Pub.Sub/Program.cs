83fde77ccb621352c9620b76250d398fbd33ff6d
Taha Korkmaz <taha.korkmaz@patikaglobal.com>
2025-05-08 19:16:11 +0300

    Redis.Pub.Sub.Publisher created

---
+++ Redis.Pub.Sub/Program.cs
@@ -0,0 +1,19 @@
+﻿using StackExchange.Redis;
+namespace Redis.Pub.Sub
+{
+    internal class Program
+    {
+        static async Task Main(string[] args)
+        {
+           ConnectionMultiplexer connection = await  ConnectionMultiplexer.ConnectAsync("localhost:6379");
+            ISubscriber subscriber = connection.GetSubscriber();
+
+            while (true)
+            {
+                Console.Write("Mesaj : ");
+                string message = Console.ReadLine();
+                await subscriber.PublishAsync("mychannel", message);
+            }
+        }
+    }
+}
